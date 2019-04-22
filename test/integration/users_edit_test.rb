78f87e4f73b545fceb4c46a278dbe700237d3ae8
rudyNK <rudy@noukod.com>
2019-04-22 14:37:27 -0400

    Add user microposts

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user = users(:rudy)
   end
 
   test "unsuccessful edit" do
